d52892f948ac4981feb8005e064bf07625a229ea
Ceci1970 <delfino.cecilia@gmail.com>
2021-12-07 12:36:32 -0300

    alumnos.jsp

--- Proyecto_Final/escuelaDigital/src/main/java/config/Conexion.java
+++ Proyecto_Final/escuelaDigital/src/main/java/config/Conexion.java
@@ -5,12 +5,12 @@ import java.sql.*;
 
 public class Conexion {
     public String driver = "com.mysql.cj.jdbc.Driver";
-    public Connection getConection(){
+    public Connection getConection(){//creamos una funcionde la clase connection que nos captura la conexion, vamos a retornar un objeto del tipo conexion
             
     
-        Connection conexion = null;
-        try{
-            Class.forName(driver);
+        Connection conexion = null;//creamos un objeto
+        try{// lo usamos pq trabajamos con base de datos, maneja los errores
+            Class.forName(driver);// es una clase del api
             conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/comision1109",
                     "root", "");
     }catch(ClassNotFoundException | SQLException e){
